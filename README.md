3c3c5d5357f76b4814b72b0dcf1c74aff1ce3e9a
Paul Herron <contact@paulherron.net>
2016-12-11 18:21:42 +0000

    Use flask to serve up status as JSON.

---
+++ README.md
@@ -0,0 +1,11 @@
+Install
+-------
+
+    pip install lifxlan
+    pip install flask
+
+
+Usage
+-----
+
+    python status.py
---
+++ serve_status.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, Response
+from lifxlan import LifxLAN
+import sys
+app = Flask(__name__)
+
+@app.route("/")
+def status():
+    num_lights = None
+    if len(sys.argv) == 2:
+        num_lights = int(sys.argv[1])
+
+    # instantiate LifxLAN client, num_lights may be None (unknown).
+    # In fact, you don't need to provide LifxLAN with the number of bulbs at all.
+    # lifx = LifxLAN() works just as well. Knowing the number of bulbs in advance 
+    # simply makes initial bulb discovery faster.
+    lifx = LifxLAN(num_lights)
+
+    devices = lifx.get_lights()
+    power_states = {}
+
+    for d in devices:
+        power_states[ d.get_label() ] = d.get_power()
+
+    return jsonify(power_states)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
